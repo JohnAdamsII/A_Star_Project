14361bf596bbaf103a78d5d78397c1c32e1d0615
John Adams <johnadams@mail.usf.edu>
2018-09-13 12:15:07 -0400

    init commit

---
+++ A_Star.py
@@ -0,0 +1,28 @@
+import Map
+from math import sqrt
+
+def calculate_heuristic(node, goal, amount_traversed, map, type):
+    estimate = 0 #had an error before is this what you wanted?
+    if type == False:
+        #get node coordinates
+        node_location = map[node]
+        x1 = float(node_location[0])
+        y1 = float(node_location[1])
+
+        #get goal coordinates
+        goal_location = map[goal]
+        x2 = float(goal_location[0])
+        y2 = float(goal_location[1])
+
+        #calculate straight line distance between node and goal
+        estimate = sqrt((x2-x1)*(x2-x1) + (y2-y1)*(y2-y1))
+    else:
+        estimate = 1
+    
+    return estimate + amount_traversed
+
+def main():
+    my_map = Map.Map("connections.txt", "locations.txt") #needed dor operator for imported files
+
+if __name__ == "__main__":
+    main()
---
+++ Map.py
@@ -0,0 +1,65 @@
+class Map():
+    """Maps are undirected graphs whose nodes have locations and connections"""
+
+    def __init__(self, connections_file_string, locations_file_string):
+        """Set up the map created by the passed in connections and locations files"""
+
+        self.connections_dictionary = {}
+        self.locations_dictionary = {}
+
+        # construct a dictionary mapping a node to its connections list
+        connections_file = open(connections_file_string, 'r')
+        for line in connections_file:
+            stripped_line = line.rstrip('\n')
+            split_line = stripped_line.split()
+            self.connections_dictionary[split_line[0]] = split_line[2:]
+        del self.connections_dictionary["END"]
+
+        # construct a dictionary mapping a node to its location
+        locations_file = open(locations_file_string, 'r')
+        for line in locations_file:
+            stripped_line = line.rstrip('\n')
+            split_line = stripped_line.split()
+            self.locations_dictionary[split_line[0]] = split_line[1:]
+        del self.locations_dictionary["END"]
+
+    def print_connections(self):
+        """Prints all nodes and their connections"""
+        for key, value in self.connections_dictionary.items():
+            print("Node: " + key + "\tConnections: %s" % value)
+
+    def print_locations(self):
+        """Prints all nodes and their locations"""
+        for key, value in self.locations_dictionary.items():
+            print("Node: " + key + "\tLocation: %s" % value)
+
+    def print_all(self):
+        """Prints all nodes' locations and connections"""
+        for key, value in self.locations_dictionary.items():
+            connections = self.connections_dictionary[key]
+            print("Node:" + key + " \t\tLocation:" + str(value) +
+                  "\t\tConnections:" + str(connections))
+
+    def deleteNode(self, node):
+        to_be_deleted = node
+        for item in self.connections_dictionary.values():
+            if to_be_deleted in item:
+                item.remove(to_be_deleted)
+        del self.connections_dictionary[node]
+        del self.locations_dictionary[node]
+
+
+def main():
+
+    my_map = Map("connections.txt", "locations.txt")
+    my_map.print_all()
+
+    print("************************************************************")
+
+    my_map2 = Map("connections.txt", "locations.txt")
+    my_map2.deleteNode("A1")
+    my_map2.print_all()
+
+
+if __name__ == "__main__":
+    main()
